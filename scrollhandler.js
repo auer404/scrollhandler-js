28b30cb90b66363183d4b0ddc18d5195bad4e192
auer404 <74737685+auer404@users.noreply.github.com>
2022-11-07 14:26:21 +0100

    Add a first complete version

--- scrollhandler.js
+++ scrollhandler.js
@@ -1,6 +1,8 @@
 var panels = document.querySelectorAll(".panel");
 //console.log(panels);
 
+var the_link; // A chaque scroll, cette variable globale sera mise à jour : elle fera alors référence au dernier lien ayant pris la couleur orange
+
 function scroll_handler() { // "handler" = gestionnaire
     
     // On s'assure de connaître la distance scrollée dans la fenêtre (ouvrir la console pour voir)
@@ -17,6 +19,13 @@ function scroll_handler() { // "handler" = gestionnaire
     
     // Fonctionnalité 2 : faire réagir les éléments du menu lorsque le panneau correspondant est à l'écran
     
+    /*
+    
+    Compléter cette fonctionnalité :
+    Avant que la couleur du bon lien soit changée, il faudrait s'assurer que tous les liens correspondant à un .panel perdent leur modification de couleur. On peut considérer cela comme une "remise à zero" de l'état des liens, à faire à chaque scroll (donc dans cette fonction scroll_handler). Pour être sûr d'agir sur chaque lien possiblement concerné, on peut profiter du passage en revue des .panel (depuis un .panel on peut retrouver le lien correspondant, voir plus bas).
+    
+    */
+    
     var panel_on_screen;
     
     panels.forEach(function(the_panel){
@@ -31,6 +40,20 @@ function scroll_handler() { // "handler" = gestionnaire
             
         }
         
+        // Correction Bug :
+        
+        // 1) Désigner ici le lien correspondant à ce panneau
+        // 2) Le lien peut, d'office, perdre sa stylisation de couleur (donner "" au lieu de "orange")
+        
+/* Solution 1 :
+        var link_to_deactivate = document.querySelector("a[href='#" + the_panel.id + "']");
+        link_to_deactivate.style.color = "";*/
+        
+        /* Solution 2 (à condition d'avoir déclaré the_link de manière globale) */
+        if (the_link) { // Permet de tester si the_link n'est pas undefined (et d'éviter un bug), ce qui sera le cas lors du tout premier scroll car une valeur n'est donnée que plus bas dans cette fonction
+            the_link.style.color = "";
+        }
+        
     });
     
     // Après le forEach, la variable panel_on_screen fait référence au bon élément : le dernier qui a pu valider le test dans le forEach
@@ -38,7 +61,7 @@ function scroll_handler() { // "handler" = gestionnaire
     var panel_id = panel_on_screen.id;
     console.log("Le .panel d'identifiant " + panel_id + " est à l'écran");
     
-    var the_link = document.querySelector("a[href='#" + panel_id + "']");
+    the_link = document.querySelector("a[href='#" + panel_id + "']");
     
     the_link.style.color = "orange"; // A modifier : il y a plus élégant...
     
